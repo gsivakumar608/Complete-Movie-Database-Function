a6320c0b344f84671bafc097dd1ca0e76eb1f51a
VENKATA SIVAKUMAR GANDIKOTA <gsivakumar.608@gmail.com>
2016-07-11 14:38:06 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(37, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(296, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(37, 157);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(229, 321);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(510, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(37, 86);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(296, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(308, 189);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(503, 71);
+            this.listView2.TabIndex = 6;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(308, 314);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(122, 147);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listView3
+            // 
+            this.listView3.Location = new System.Drawing.Point(481, 314);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(278, 147);
+            this.listView3.TabIndex = 8;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Movie Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Movie Detail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 298);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Genres";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(478, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Makers";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 506);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        private DataClasses1DataContext database = new DataClasses1DataContext();
+        private string selectedMenu = "";
+        private Dictionary<int, string> movieMap = new Dictionary<int, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+           // database.Log = Console.Out;
+            loadCombox();
+            listView1.View = View.List;
+            listView2.View = View.Details;
+            listView3.View = View.Details;
+            listView2.Columns.Add("Id");
+            listView2.Columns.Add("Title");
+            listView2.Columns.Add("Release year");
+            listView2.Columns.Add("Language");
+            listView2.Columns.Add("Rating");
+            listView3.Columns.Add("Name");
+            listView3.Columns.Add("Role");
+            MessageBox.Show("Select the criteria from the combox box and enter the value in textbox press submit");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            movieMap.Clear();
+            string value = textBox1.Text;
+            string value1 = textBox2.Text;
+            string selectedMenu1 = comboBox2.SelectedItem.ToString();
+            List<int> g = getResults(selectedMenu, value);
+
+            Console.WriteLine(g.Count);
+
+            List<int> h = getResults(selectedMenu1, value1);
+      
+            IEnumerable<int> nm = new List<int>();
+
+            if (h.Count > 0)
+                nm = g.Intersect(h);
+            else
+                nm = g;
+           
+
+            IQueryable<MOVy> oDataQuery = database.MOVies;
+      
+            
+            foreach (int a in nm)
+            {
+                var query = from m in database.MOVies where m.MOVIE_ID == a select m;
+                var arr = query.ToArray();
+                foreach (var record in arr)
+                {
+                    movieMap.Add(record.MOVIE_ID,record.TITLE);
+                }
+            }
+
+
+            foreach (var s in movieMap)
+            {
+                ListViewItem n = new ListViewItem(new string[] { s.Value, s.Key.ToString()});
+                listView1.Items.Add(n);
+            }
+     
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+
+            selectedMenu = comboBox1.SelectedItem.ToString();
+            List<string> menu = new List<string>();
+            menu.Add("Director");
+            menu.Add("Actor");
+            menu.Add("Realese year range");
+            menu.Add("Language");
+            menu.Add("Genre");
+            menu.Add("Title");
+            menu.Add("Rating");
+
+            if (selectedMenu.Equals("Director"))
+                menu.Remove("Director");
+            else if (selectedMenu.Equals("Actor"))
+                menu.Remove("Actor");
+            else if(selectedMenu.Equals("Realese year range"))
+                menu.Remove("Actor");
+            else if(selectedMenu.Equals("Language"))
+                menu.Remove("Language");
+            else if(selectedMenu.Equals("Genre"))
+                menu.Remove("Genre");
+            else if(selectedMenu.Equals("Title"))
+                menu.Remove("Title");
+            else if(selectedMenu.Equals("Rating"))
+                menu.Remove("Rating");
+
+            comboBox2.DataSource = menu;
+            comboBox2.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            comboBox2.AutoCompleteSource = AutoCompleteSource.ListItems;
+
+        }
+
+        private void loadCombox()
+        {
+            List<string> menu = new List<string>();
+
+            menu.Add("Director");
+            menu.Add("Actor");
+            menu.Add("Realese year range");
+            menu.Add("Language");
+            menu.Add("Genre");
+            menu.Add("Title");
+            menu.Add("Rating");
+
+            comboBox1.DataSource = menu;
+            comboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            comboBox1.AutoCompleteSource = AutoCompleteSource.ListItems;
+
+        }
+
+
+
+        private void textBox1_KeyDown(object sender, EventArgs e)
+        {
+
+            if (selectedMenu.StartsWith("Rea"))
+            {
+                if (textBox1.TextLength == 4)
+                {
+                    string val = textBox1.Text;
+                    textBox1.Text = val + ",";
+                    textBox1.Select(textBox1.TextLength, 0);
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private List<int> getResults(String selectedMenu, String value)
+        {
+            List<int> result = new List<int>();
+
+            if (selectedMenu.Equals("Actor") || selectedMenu.Equals("Director"))
+            {
+                var query = (from MovieMaker in database.MOVIEMAKERs where MovieMaker.MOVIE_ROLE == selectedMenu && MovieMaker.MAKER_ID == (from Maker in database.MAKERs where Maker.MAKER_NAME == value select Maker.MAKER_ID).FirstOrDefault() select MovieMaker.MOVIE_ID);
+                result = query.ToList();
+            }
+            else if (selectedMenu.Equals("Genre"))
+            {
+                var query = (from genre in database.GENREs where genre.GENRE1 == value select genre.MOVIE_ID);
+                result = query.ToList();
+            }
+
+            else if (selectedMenu.Equals("Rating"))
+            {
+                var query = (from movies in database.MOVies where movies.RATING == int.Parse(value) select movies.MOVIE_ID);
+                result = query.ToList();
+            }
+            else if (selectedMenu.Equals("Language"))
+            {
+                var query = (from movies in database.MOVies where movies.SPOKEN_LANGUAGE == value select movies.MOVIE_ID);
+                result = query.ToList();
+            }
+            else if (selectedMenu.Equals("Title"))
+            {
+                var query = (from movies in database.MOVies where movies.TITLE.Contains(value) select movies.MOVIE_ID);
+                result = query.ToList();
+            }
+            else if (selectedMenu.StartsWith("Rea"))
+            {
+                string[] range = value.Split(',');
+                var query = (from movies in database.MOVies where movies.RELEASE_YEAR >= int.Parse(range[0]) && movies.RELEASE_YEAR <= int.Parse(range[1]) select movies.MOVIE_ID);
+                result = query.ToList();
+            }
+            return result;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+   
+            if (listView1.SelectedItems.Count > 0)
+            {
+                listView2.Items.Clear();
+                listView3.Items.Clear();
+                List<string> genreList = new List<string>();
+                var selectedItem = listView1.SelectedItems[0];
+                int id = int.Parse(selectedItem.SubItems[1].Text);
+                var query = from genre in database.GENREs where genre.MOVIE_ID == id select genre;
+                var queryArr = query.ToArray();
+               
+                foreach (var quer in queryArr)
+                {
+                    genreList.Add(quer.GENRE1);
+                }
+                listBox1.DataSource = genreList;
+
+                
+                var que = from m in database.MOVies where m.MOVIE_ID == id select m;
+                    var arr = que.ToArray();
+                    foreach (var a in arr)
+                    {
+                        ListViewItem movieDetail = new ListViewItem(new string[]{a.MOVIE_ID.ToString(),a.TITLE,a.RELEASE_YEAR.ToString(),a.SPOKEN_LANGUAGE,a.RATING.ToString()});
+                        listView2.Items.Add(movieDetail);
+                    }
+
+
+                    var que1 = from m in database.MOVIEMAKERs where m.MOVIE_ID == id select m;
+                    var arr1 = que1.ToArray();
+                    foreach (var a in arr1)
+                    {
+                        var que2 = from m in database.MAKERs where m.MAKER_ID == a.MAKER_ID select m.MAKER_NAME;
+                        foreach(var v in que2) {
+                             ListViewItem makerDetail = new ListViewItem(new string[] { v, a.MOVIE_ROLE });
+                             listView3.Items.Add(makerDetail);
+                        }
+                       
+                    }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listView3.Items.Clear();
+            listBox1.Items.Clear();
+        }
+    }
+}
